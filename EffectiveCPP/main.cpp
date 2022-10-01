9e9d8587013bf5ae98b695beede1f0d0bea6e3b3
kotomi <yingcihtplus@163.com>
2022-10-01 12:37:55 +0800

    Initial Commit

---
+++ EffectiveCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  EffectiveCPP
+//
+//  Created by 舞 on 2022/10/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
